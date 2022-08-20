7f472e3c6628bb2c5cc85b2403d23a51617455db
Thang Luong <lhthang96@gmail.com>
2022-08-20 11:17:03 +0700

    fix: added react query, replaced render with createRoot.render from react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
-render(
+const root = createRoot(document.getElementById('app'));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('app')
+  </BrowserRouter>
 );
