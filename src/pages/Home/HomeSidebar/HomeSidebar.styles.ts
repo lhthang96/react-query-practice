80f28de31a6733c00c197fffc8c0833c404325d3
Thang Luong <lhthang96@gmail.com>
2022-09-09 21:08:42 +0700

    fix: fixed padding of home sidebar component

--- src/pages/Home/HomeSidebar/HomeSidebar.styles.ts
+++ src/pages/Home/HomeSidebar/HomeSidebar.styles.ts
@@ -4,7 +4,7 @@ export const StyledHomeSidebar = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden auto;
-  padding: 24px 24px 32px 24px;
+  padding: 24px 24px 16px 24px;
   display: flex;
   flex-direction: column;
   row-gap: 40px;
